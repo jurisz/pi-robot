ae7f00e10fc96338463c2d267e4453928aa0bb3e
juris <juris.zubkans@gmail.com>
2019-12-15 14:17:04 +0200

    some image processing tests to get lines

---
+++ test-samples/houhgline-transform.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
+
+def automatic_brightness_and_contrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index - 1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum / 100.0)
+    clip_hist_percent /= 2.0
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size - 1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
+    plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return auto_result, alpha, beta
+
+
+img = cv2.imread('../1/sample-2019-12-15_06:57-2.jpg')
+contrasted, alpha, beta = automatic_brightness_and_contrast(img, 1)
+gray = cv2.cvtColor(contrasted, cv2.COLOR_BGR2GRAY)
+# edges = cv2.Canny(gray, 50, 500, apertureSize=3)
+
+edges = auto_canny(img)
+
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+
+print(lines)
+# for rho, theta in lines[0]:
+#     a = np.cos(theta)
+#     b = np.sin(theta)
+#     x0 = a * rho
+#     y0 = b * rho
+#     x1 = int(x0 + 1000 * (-b))
+#     y1 = int(y0 + 1000 * (a))
+#     x2 = int(x0 - 1000 * (-b))
+#     y2 = int(y0 - 1000 * (a))
+#
+#     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# cv2.imwrite('houghlines3.jpg', img)
+
+# cv2.imshow('image', img)
+# cv2.imshow('image', gray)
+cv2.imshow('image', edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
